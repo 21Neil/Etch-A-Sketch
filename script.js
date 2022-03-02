a5a0302dd9cb93345812d54c6d24908b9f63dba5
Neil <88711171+21Neil@users.noreply.github.com>
2022-03-02 15:58:52 +0800

    Add painting function

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const container = document.querySelector('.container');
+
 for(let i = 0; i < 256; i++) {
   const div = document.createElement('div');
-  div.setAttribute('class', 'square')
+  div.addEventListener('mouseenter', painting)
   container.appendChild(div)
+}
+
+function painting(e) {
+  e.path[0].setAttribute('class', 'black')
 }
