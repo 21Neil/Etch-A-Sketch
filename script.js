1dd746bc52070fd91d2470f0a67300a1e46b81a3
Neil <88711171+21Neil@users.noreply.github.com>
2022-03-02 17:25:08 +0800

    Add askGrid removeBoard createBoard function to button

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
-const container = document.querySelector('.container');
-const clearBtn = document.querySelector('.clearBtn');
+const container = document.querySelector(".container");
+const clearBtn = document.querySelector(".clearBtn");
+let sideLength = 16;
 
-clearBtn.addEventListener('click', clear)
+createBoard();
 
-for(let i = 0; i < 256; i++) {
-  const div = document.createElement('div');
-  div.addEventListener('mouseenter', painting)
-  container.appendChild(div)
+clearBtn.addEventListener("click", () => {
+  if(askGrid() === 'ERROR'){
+    return;
+  }
+  clear();
+  removeBoard();
+  createBoard();
+});
+
+function createBoard() {
+  for (let i = 0; i < sideLength ** 2; i++) {
+    const div = document.createElement("div");
+    div.addEventListener("mouseenter", painting);
+    container.appendChild(div);
+  }
 }
-
 function painting(e) {
-  e.path[0].setAttribute('class', 'black')
+  e.path[0].style.backgroundColor = 'black';
 }
 
 function clear() {
-  const divs = document.querySelectorAll('.container > div');
-  divs.forEach( div =>
-    div.removeAttribute('class')
-  )
+  const divs = document.querySelectorAll(".container > div");
+  divs.forEach((div) => div.removeAttribute("class"));
+}
+
+function askGrid() {
+  temp = prompt("How many squares you need per side? (max: 100)");
+  if (temp > 100) {
+    alert("Max value is 100!");
+    return 'ERROR';
+  } else if (temp === null) {
+    return 'ERROR';
+  } else {
+    sideLength = temp;
+    container.style.gridTemplateColumns = `repeat(${sideLength}, 1fr)`;
+    createBoard();
+  }
+}
+
+function removeBoard() {
+  const divs = document.querySelectorAll(".container > div");
+  divs.forEach((div) => container.removeChild(div));
 }
