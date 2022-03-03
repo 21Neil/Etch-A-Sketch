63350004e0bf5ec446936debbdb3432288186775
Neil <88711171+21Neil@users.noreply.github.com>
2022-03-03 14:26:19 +0800

    Add black painting opacity

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const container = document.querySelector(".container");
 const clearBtn = document.querySelector(".clearBtn");
+let isMouseDown = false;
 
 clearBtn.addEventListener("click", () => {
-  if(askGrid() === 'ERROR'){
+  if (askGrid() === "ERROR") {
     return;
   }
   clear();
@@ -13,7 +14,15 @@ clearBtn.addEventListener("click", () => {
 function createBoard(size) {
   for (let i = 0; i < size * size; i++) {
     const div = document.createElement("div");
-    div.addEventListener("mouseenter", paintingBlack);
+    div.addEventListener("mousedown", (e) => {
+      paintingBlack(e);
+      isMouseDown = true;
+    });
+    div.addEventListener("mouseup", () => (isMouseDown = false));
+    div.addEventListener("mouseenter", (e) => {
+      if (isMouseDown) paintingBlack(e);
+    });
+    div.addEventListener("mousedown", (e) => e.preventDefault());
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.appendChild(div);
   }
@@ -22,14 +31,33 @@ function createBoard(size) {
 createBoard(16);
 
 function paintingBlack(e) {
-  e.path[0].style.backgroundColor = 'black';
+  e.target.style.backgroundColor = `rgb(0, 0, 0, ${setOpacity(
+    e.target.style.backgroundColor
+  )})`;
+}
+
+function setOpacity(backgroundColor) {
+  if (!backgroundColor) {
+    return 0.1;
+  } else {
+    let opacity = backgroundColor.slice(
+      backgroundColor.indexOf(".") - 1,
+      backgroundColor.length - 1
+    );
+    opacity = +opacity;
+    if (opacity < 1 && opacity !== 0){
+      console.log(opacity)
+      return  opacity += 0.1;
+    }
+  }
 }
 
 function paintingColor(e) {
-  let r = Math.floor(Math.random() * 256);
-  let g = Math.floor(Math.random() * 256);
-  let b = Math.floor(Math.random() * 256);
-  e.path[0].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  e.path[0].style.backgroundColor = `rgb(
+    ${Math.floor(Math.random() * 256)}, 
+    ${Math.floor(Math.random() * 256)}, 
+    ${Math.floor(Math.random() * 256)}
+    )`;
 }
 
 function clear() {
@@ -41,9 +69,9 @@ function askGrid() {
   size = prompt("How many squares you need per side? (max: 100)");
   if (size > 100) {
     alert("Max value is 100!");
-    return 'ERROR';
+    return "ERROR";
   } else if (size === null) {
-    return 'ERROR';
+    return "ERROR";
   } else {
     return size;
   }
