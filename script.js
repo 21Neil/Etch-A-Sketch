9c83705418860c4951ff233f396c86bf778e70fd
Neil <88711171+21Neil@users.noreply.github.com>
2022-03-03 09:58:39 +0800

    Change function createBoard logic & add background color

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const container = document.querySelector(".container");
 const clearBtn = document.querySelector(".clearBtn");
-let sideLength = 16;
-
-createBoard();
 
 clearBtn.addEventListener("click", () => {
   if(askGrid() === 'ERROR'){
@@ -10,16 +7,20 @@ clearBtn.addEventListener("click", () => {
   }
   clear();
   removeBoard();
-  createBoard();
+  createBoard(size);
 });
 
-function createBoard() {
-  for (let i = 0; i < sideLength ** 2; i++) {
+function createBoard(size) {
+  for (let i = 0; i < size * size; i++) {
     const div = document.createElement("div");
     div.addEventListener("mouseenter", painting);
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.appendChild(div);
   }
 }
+
+createBoard(16);
+
 function painting(e) {
   e.path[0].style.backgroundColor = 'black';
 }
@@ -30,16 +31,14 @@ function clear() {
 }
 
 function askGrid() {
-  temp = prompt("How many squares you need per side? (max: 100)");
-  if (temp > 100) {
+  size = prompt("How many squares you need per side? (max: 100)");
+  if (size > 100) {
     alert("Max value is 100!");
     return 'ERROR';
-  } else if (temp === null) {
+  } else if (size === null) {
     return 'ERROR';
   } else {
-    sideLength = temp;
-    container.style.gridTemplateColumns = `repeat(${sideLength}, 1fr)`;
-    createBoard();
+    return size;
   }
 }
 
